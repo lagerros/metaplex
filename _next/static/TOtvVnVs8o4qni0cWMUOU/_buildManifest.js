4f4d178bc4bc28df139a1d9444c581d5dd69f069
lagerros <jacobklagerros@gmail.com>
2021-10-09 14:49:01 -0700

    Updates

---
+++ _next/static/TOtvVnVs8o4qni0cWMUOU/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[{source:"/:any*",destination:"/"}],fallback:[]},"/":["static/chunks/pages/index-317f0945e25381ad9e4c.js"],"/_error":["static/chunks/pages/_error-e1bcaa043b7e8bc8fc85.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
